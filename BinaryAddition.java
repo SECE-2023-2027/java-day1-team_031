8454532a7e12fddbc58faa4369bea62bf05087f8
Haarish guru <155081356+Haarishcsbs016@users.noreply.github.com>
2026-06-15 21:39:32 +0530

    Assignment finished

--- BinaryAddition.java
+++ BinaryAddition.java
@@ -3,14 +3,10 @@ public class BinaryAddition {
 
         String binary1 = "100010";
         String binary2 = "110010";
-
-        int num1 = Integer.parseInt(binary1, 2);
+        int num1 = Integer.parseInt(binary1,2);
         int num2 = Integer.parseInt(binary2, 2);
-
         int sum = num1 + num2;
-
         String result = Integer.toBinaryString(sum);
-
         System.out.println("Input first binary number: " + binary1);
         System.out.println("Input second binary number: " + binary2);
         System.out.println("Sum of two binary numbers: " + result);
