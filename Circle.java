0d2a933b116e87aecb0e9bf8fce45c2bd170ca93
Haarish guru <155081356+Haarishcsbs016@users.noreply.github.com>
2026-06-15 16:02:29 +0530

    Added Circle program

---
+++ Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+    public static void main(String[] args) {
+
+        double radius = 5;
+
+        double area = 3.14 * radius * radius;
+        double perimeter = 2 * 3.14 * radius;
+
+        System.out.println("Radius = " + radius);
+        System.out.println("Area of Circle = " + area);
+        System.out.println("Perimeter of Circle = " + perimeter);
+    }
+}
