46463bb81b283c4879aa80c50e9f312a7664aa3c
Haarish guru <155081356+Haarishcsbs016@users.noreply.github.com>
2026-06-15 21:41:56 +0530

    Assignment finished

--- Prob_7.java
+++ Prob_7.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Prob {
+public class Prob_7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input a Decimal Number: ");
